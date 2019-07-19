2b374c169ba8688e5950395104811a580bc6e2f7
marutilm <maruti.madesur@siemens-healthineers.com>
2019-07-19 11:15:32 +0530

    supermath v3

--- supermath.cs
+++ supermath.cs
@@ -1,6 +1,9 @@
 public class SuperMath {
 public int Add(int i, int j) {
 return i+j;
- } public int Add(int i, int j)
+ } public int product(int i, int j) {
  return i+j;
+ } public int substract(int i, int j) {
+	 return i-j;
+ }
 }
