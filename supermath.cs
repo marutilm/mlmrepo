2294400d2f69ab19626ca9f7c0e38b92ad8506ab
marutilm <53069707+marutilm@users.noreply.github.com>
2019-07-19 12:27:48 +0530

    exchanged the integer valuse (Supermath v4)
    Supermath v4

--- supermath.cs
+++ supermath.cs
@@ -3,7 +3,7 @@ public int Add(int i, int j) {
 return i+j;
  } public int product(int i, int j) {
  return j+i;
- } public int substract(int j, int j) {
+ } public int substract(int j, int i) {
 	 return j-i;
  }
 }
