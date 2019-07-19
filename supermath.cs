e3e8f4a44b0bc9537ed3ecea2cd832ad2f68b3a3
marutilm <53069707+marutilm@users.noreply.github.com>
2019-07-19 13:46:14 +0530

    Supermath V5

--- supermath.cs
+++ supermath.cs
@@ -2,7 +2,7 @@ public class SuperMath {
 public int Add(int i, int j) {
 return j+i;
  } public int product(int i, int j) {
- return j+i;
+ return j*i;
  } public int substract(int j, int i) {
 	 return j-i;
  }
